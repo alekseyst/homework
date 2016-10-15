7f4b1a8eb533c5316faa063a729c36bec71e81e8
avdavidenko <davidenkonastya98@gmail.com>
2016-10-15 10:20:44 +0300

    Add files via upload

---
+++ pr_if.py
@@ -0,0 +1,9 @@
+a = int (input ("Введите число a: "))
+b = int (input ("Введите число b: "))
+c = int (input ("Введите число c: "))
+if (a/b == c):
+    print ("a / b = c")
+else: print ("a / b != c")
+if (a ** b == c):
+    print ("a ^ b = c")
+else: print ("a ^ b != c")
